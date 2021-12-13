ac109fa3f64dc2519c5d5cf1a9108e513c0838cf
Kévin JOSE <kevin.jose.92@gmail.com>
2021-12-13 17:11:54 +0100

    add fields

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ const preStyle = {
   whiteSpace: "pre-wrap",
 }
 
-const FSApiCall = ({vid}) => {
+const FSApiCall = ({vid, apikey, envid}) => {
   const [response, setResponse] = useState("");
 
-  fetch("https://kevin-flagship-api.herokuapp.com/api?vid=" + vid).then(r => {
+  fetch("https://kevin-flagship-api.herokuapp.com/api?vid=" + vid + "&apikey=" + apikey + "&envid=" + envid).then(r => {
     r.text().then(data => {
       setResponse(data);
     })
@@ -46,14 +46,14 @@ const FSComponent = () => {
   );
 };
 
-const FSCall = ({vid}) => {
+const FSCall = ({envid, apikey, vid}) => {
 
   return (
     <>
       <h2>FS response called by Front</h2>
       <FlagshipProvider
-        envId="bvvol5cmicqk8sigckng"
-        apiKey="d5UIKzy8Lp58DuASSGmS3dHECjXtpus1mHRtdwe7" // <= Required in next major release
+        envId={envid}
+        apiKey={apikey} // <= Required in next major release
         visitorData={{
           id: vid,
           context: {
@@ -72,10 +72,14 @@ const FSCall = ({vid}) => {
 
 
 function App() {
+  const [envid, setEnvID] = useState("0");
+  const [apikey, setApiKey] = useState("abc");
   const [vid, setVID] = useState("0");
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setEnvID(event.target.elements.envid.value);
+    setApiKey(event.target.elements.apikey.value);
     setVID(event.target.elements.vid.value);
   }
 
@@ -85,7 +89,21 @@ function App() {
 
       <form onSubmit={handleSubmit}>
         <label>
-          Visitor ID: 
+          Env ID: 
+          (chosen : {envid})
+          <br/>
+
+          <input type="text" name="envid" />
+        </label><br/>
+        <label>
+          API key: 
+          (chosen : {apikey})
+          <br/>
+
+          <input type="text" name="apikey" />
+        </label><br/>
+        <label>
+          Visitor ID:  
           (chosen : {vid})
           <br/>
 
@@ -94,8 +112,8 @@ function App() {
         <input type="submit" value="Submit" />
       </form>
 
-      <FSCall vid={vid} />
-      <FSApiCall vid={vid} />
+      <FSCall envid={envid} apikey={apikey} vid={vid} />
+      <FSApiCall envid={envid} apikey={apikey} vid={vid} />
       
     </div>
   );
